8bd7f99c4508072d0da79799c2c89797b37e2cdf
Alex Wilkerson <alex@uncorrected.com>
2017-04-14 16:55:38 -0500

    fixed culour module to not be so complicated and to work with scrolling windows.

--- culour.py
+++ culour.py
@@ -53,60 +53,46 @@ def _color_str_to_color_pair(color):
     return color_pair
 
 
-def _add_line(y, x, window, line):
+def addstr(window, string):
     # split but \033 which stands for a color change
-    color_split = line.split('\033')
+    color_split = string.split('\033')
 
     # Print the first part of the line without color change
     default_color_pair = _get_color(-1, -1)#curses.COLOR_WHITE, curses.COLOR_BLACK)
-    window.addstr(y, x, color_split[0], curses.color_pair(default_color_pair))
-    x += len(color_split[0])
+    window.addstr(color_split[0], curses.color_pair(default_color_pair))
 
     # Iterate over the rest of the line-parts and print them with their colors
     for substring in color_split[1:]:
         color_str = substring.split('m')[0]
         substring = substring[len(color_str)+1:]
         color_pair = _color_str_to_color_pair(color_str)
-        window.addstr(y, x, substring, curses.color_pair(color_pair))
-        x += len(substring)
+        window.addstr(substring, curses.color_pair(color_pair))
+
+#  def _inner_addstr(window, string, y=-1, x=-1, nl=0):
+#      assert curses.has_colors(), "Curses wasn't configured to support colors. Call curses.start_color()"
+
+#      if nl == 1:
+#          window.addstr('\n')
+#      cur_y, cur_x = window.getyx()
+#      if y == -1:
+#          y = cur_y
+#      if x == -1:
+#          x = cur_x
+#      for line in string.split(os.linesep):
+#          _add_line(y, x, window, line)
+#          # next line
+#          y += 1
 
 
-def _inner_addstr(window, string, y=-1, x=-1):
-    assert curses.has_colors(), "Curses wasn't configured to support colors. Call curses.start_color()"
+#  def addstr(window, string):
+#      """
+#      Adds the color-formatted string to the given window, in the given coordinates
+#      To add in the current location, call like this:
+#          addstr(window, string)
+#      and to set the location to print the string, call with:
+#          addstr(window, y, x, string)
+#      Only use color pairs up to 100 when using this function,
+#      otherwise you will overwrite the pairs used by this function
+#      """
 
-    cur_y, cur_x = window.getyx()
-    if y == -1:
-        y = cur_y
-    if x == -1:
-        x = cur_x
-    for line in string.split(os.linesep):
-        _add_line(y, x, window, line)
-        # next line
-        y += 1
-
-
-def addstr(*args):
-    """
-    Adds the color-formatted string to the given window, in the given coordinates
-    To add in the current location, call like this:
-        addstr(window, string)
-    and to set the location to print the string, call with:
-        addstr(window, y, x, string)
-    Only use color pairs up to 100 when using this function,
-    otherwise you will overwrite the pairs used by this function
-    """
-    if len(args) != 2 and len(args) != 4:
-        raise TypeError("addstr requires 2 or 4 arguments")
-
-    if len(args) == 4:
-        window = args[0]
-        y = args[1]
-        x = args[2]
-        string = args[3]
-    else:
-        window = args[0]
-        string = args[1]
-        y = -1
-        x = -1
-
-    return _inner_addstr(window, string, y, x)
+#      return _inner_addstr(window, string)
